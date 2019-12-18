c0e42c05b4a44225ccda7aa9706b971125616b7c
Dao Van Cuong <daovancuong@Daos-MacBook-Pro.local>
2019-12-18 15:01:00 +0700

    update state input and fix bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      todoList: []
+      todoList: [],
+      todo: ""
     };
-    this.onSubmit = this.onSubmit.bind(this)
-    this.addNewItem = this.addNewItem.bind(this)
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   // componentDidMount() {
@@ -17,49 +17,47 @@ class App extends Component {
   //   this.setState({todoList: listToDo});
   // }
 
-  onSubmit = (e) => {
+  onSubmit = e => {
     e.preventDefault();
-    var newItemValue = this.refs.itemName.value;
-    
-    if(newItemValue) {
-      this.addNewItem({
-        item: newItemValue,
-        done: false
-      })
-    }
-  }
-
-  addNewItem = (item) => {
-    let arr = this.state.todoList;
-    arr.push(item)
-    this.setState({todoList: arr});
-  }
+    let todoList = this.state.todoList;
+    todoList.push(this.state.todo);
+    this.setState({ todoList: todoList });
+  };
 
   render() {
-    let items = this.state.todoList
-  
-    const listItems = items.map((item, index) => (
-      <li className="list-group-item " key={index}>
-        <div className="">
-          <span className="glyphicon glyphicon-ok icon" aria-hidden="true">
-            {item}
-          </span>
-          <button type="button" className="mark-done" onClick={this.onClickDone}>&radic;</button>
-          <button type="button" className="close" onClick={this.onClickClose}>&times;</button>
-        </div>
-      </li>
-    ));
+    let items = this.state.todoList;
 
     return (
       <div id="main">
         <h1>Todo list</h1>
-        <ul className="list-group">
-          {listItems}
-        </ul>
         <form ref="form" onSubmit={this.onSubmit} className="form-inline">
-          <input type="text" ref="itemName" className="form-control" placeholder="add a new todo..."/>
-          <button type="submit" className="btn btn-primary">Add</button> 
+          <input
+            type="text"
+            ref="itemName"
+            className="form-control"
+            placeholder="add a new todo..."
+            value={this.state.todo}
+            onChange={event => {
+              this.setState({ todo: event.target.value });
+            }}
+          />
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
         </form>
+        {items.map((item, index) => (
+          <li className="list-group-item " key={index}>
+            <div className="">
+              {item}
+              <button type="button" className="mark-done">
+                &radic;
+              </button>
+              <button type="button" className="close">
+                &times;
+              </button>
+            </div>
+          </li>
+        ))}
       </div>
     );
   }
