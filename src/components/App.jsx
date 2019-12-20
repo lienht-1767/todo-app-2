98c734db0d4dac530e86e346f5e42b73edd416e8
lienht-1767 <hoang.thi.lien@sun-asterisk.com>
2019-12-20 14:39:38 +0700

    add todo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,68 @@
 import React, { Component } from "react";
 import * as actions from '../actions/index';
+import { connect } from 'react-redux';
 
 class App extends Component {
   constructor(props) {
     super(props);
   }
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo({ text: e.target[0].value, done: false});
+    e.target[0].value = ''
+  }
+
   render() {
+    const { listTodos } = this.props;
+
+    const list = listTodos.map((item, index) => (
+      <li className="list-group-item " key={index}>
+        <div className={item.done === false ? "undone" : "done"}>
+          {item.text}
+          <button type="button" className="mark-done" onClick={this.onClickMarkDone} value={index}>
+            &radic;
+          </button>
+          <button type="button" className="close" onClick={this.onClickClose} value={index}>
+            &times;
+          </button>
+        </div>
+      </li>
+    ));
+
     return (
       <div id="main">
         <h1>Todo list</h1>
-        <form ref="form" className="form-inline">
+        <form ref="form" className="form-inline" onSubmit={this.onSubmit}>
           <input
             type="text"
             ref="itemName"
             className="form-control"
             placeholder="add a new todo..."
           />
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary" >
             Add
           </button>
         </form>
+        {list}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const listTodos = state.todos;
+  return {
+    listTodos
+  };
+};
+
+const mapDistachToProps = () => dispatch => {
+  return {
+    addTodo: data => {
+      dispatch(actions.addTodo(data));
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDistachToProps)(App);
--- src/index.js
+++ src/index.js
@@ -11,11 +11,20 @@ import './index.css';
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App'
 
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer)
+const reduxDevTools =
+  (window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+  compose;
+
+let store = createStore(
+  rootReducer,
+  compose(applyMiddleware(), reduxDevTools)
+);
 
 render(
   <Provider store={store}>
